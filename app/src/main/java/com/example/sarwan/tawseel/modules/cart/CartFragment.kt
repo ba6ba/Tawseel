750f704d9ccdb303bc3891b7331b8ef2a5682975
Abdul Basit Malik <malikbasit12345@gmail.com>
2019-07-08 21:16:22 +0500

    layout designs completed

---
+++ app/src/main/java/com/example/sarwan/tawseel/modules/cart/CartFragment.kt
@@ -0,0 +1,16 @@
+package com.example.sarwan.tawseel.modules.cart
+
+import android.os.Bundle
+import android.view.View
+import com.example.sarwan.tawseel.R
+import com.example.sarwan.tawseel.base.BaseFragment
+import com.example.sarwan.tawseel.repository.main.CartRepository
+import com.example.sarwan.tawseel.repository.main.NotificationsRepostiory
+import com.example.sarwan.tawseel.repository.main.VendorRepository
+
+class CartFragment : BaseFragment<CartRepository>(R.layout.fragment_cart) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
